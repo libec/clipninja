fc508a79a0399581d24b0c70b892a8501378be1e
Libec <libor.huspenina@gmail.com>
2021-10-22 12:35:47 +0200

    Display status menu, use app as agent, properly display main window

--- App/ClipNinjaApp.swift
+++ App/ClipNinjaApp.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 import AppStart
 
-let appStart = AppStart()
-
 @main
 struct ClipNinjaApp: App {
+
+    let appStart = AppStart()
+
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             RootView(appStart: appStart)
+                .onAppear {
+                    NSApp.activate(ignoringOtherApps: true)
+                }
         }
+        .windowStyle(HiddenTitleBarWindowStyle())
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var statusItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+
+        let contentView = VStack {
+            Text("Show Clipboard")
+            Text("Preferences")
+            Text("Clear all")
+            Text("Quit")
+        }
+
+        let view = NSHostingView(rootView: contentView)
+
+        // Don't forget to set the frame, otherwise it won't be shown.
+        view.frame = NSRect(x: 0, y: 0, width: 200, height: 200)
+
+        let menuItem = NSMenuItem()
+        menuItem.view = view
+
+        let menu = NSMenu()
+        menu.addItem(menuItem)
+
+        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        self.statusItem?.menu = menu
+        self.statusItem?.button?.title = "ClipNinja"
     }
 }
