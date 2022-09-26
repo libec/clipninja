7d5446d710f41f8b75e9b16e44444e278d515fe6
Libec <libor.huspenina@gmail.com>
2022-09-26 18:16:10 +0200

    Add InMemoryViewPortRepository

--- ClipNinja/Sources/Features/Clipboard/Domain/Repository/ViewPortRepository.swift
+++ ClipNinja/Sources/Features/Clipboard/Domain/Repository/ViewPortRepository.swift
@@ -5,3 +5,20 @@ protocol ViewPortRepository {
     func update(position: Int)
     var lastPosition: Int { get }
 }
+
+final class InMemoryViewPortRepository: ViewPortRepository {
+
+    private let subject = CurrentValueSubject<Int, Never>(0)
+
+    var position: AnyPublisher<Int, Never> {
+        subject.eraseToAnyPublisher()
+    }
+
+    var lastPosition: Int {
+        subject.value
+    }
+
+    func update(position: Int) {
+        subject.value = position
+    }
+}
