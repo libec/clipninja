a2dbed82bc8f6e12377c496b1ceb7229d6b3baee
Libec <libor.huspenina@gmail.com>
2022-10-07 19:47:20 +0200

    Reset view port when going background

--- ClipNinja/Sources/Infrastructure/Navigation/Domain/HideAppUseCase.swift
+++ ClipNinja/Sources/Infrastructure/Navigation/Domain/HideAppUseCase.swift
@@ -5,12 +5,18 @@ protocol HideAppUseCase {
 final class HideAppUseCaseImpl: HideAppUseCase {
 
     private let navigation: Navigation
+    private let viewPortRepository: ViewPortRepository
 
-    init(navigation: Navigation) {
+    init(
+        navigation: Navigation,
+        viewPortRepository: ViewPortRepository
+    ) {
         self.navigation = navigation
+        self.viewPortRepository = viewPortRepository
     }
 
     func hide() {
         navigation.hide()
+        viewPortRepository.update(position: 0)
     }
 }
--- ClipNinja/Tests/Infrastructure/Navigation/Domain/HideAppUseCaseTests.swift
+++ ClipNinja/Tests/Infrastructure/Navigation/Domain/HideAppUseCaseTests.swift
@@ -5,14 +5,31 @@ class HideAppUseCaseTests: XCTestCase {
 
     func test_it_uses_navigation_to_hide_the_app() throws {
         let navigation = NavigationSpy()
-        let sut = HideAppUseCaseImpl(navigation: navigation)
+        let sut = HideAppUseCaseImpl(
+            navigation: navigation,
+            viewPortRepository: InMemoryViewPortRepository()
+        )
 
         sut.hide()
 
         XCTAssertTrue(try XCTUnwrap(navigation.hideCalled))
     }
+
+    func test_hiding_app_moves_view_port_to_beginning() {
+        let viewPortRepository = InMemoryViewPortRepository()
+        viewPortRepository.update(position: 5)
+        let sut = HideAppUseCaseImpl(
+            navigation: NavigationDummy(),
+            viewPortRepository: viewPortRepository
+        )
+
+        sut.hide()
+
+        XCTAssertEqual(viewPortRepository.lastPosition, 0)
+    }
 }
 
+typealias NavigationDummy = NavigationSpy
 class NavigationSpy: Navigation {
 
     var hideCalled: Bool?
