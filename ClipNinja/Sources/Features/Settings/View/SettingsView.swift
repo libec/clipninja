b4caae2f1d1739ac443c0dc0042f01e6500349cc
Libec <libor.huspenina@gmail.com>
2022-10-08 10:23:43 +0200

    Add accessibility shortcuts to settings

--- ClipNinja/Sources/Features/Settings/View/SettingsView.swift
+++ ClipNinja/Sources/Features/Settings/View/SettingsView.swift
@@ -4,10 +4,21 @@ public struct SettingsView: View {
 
     init() { }
 
+    private var accessibilityUrl: URL? {
+        let privacyUrlString = "x-apple.systempreferences:com.apple.preference.security?Privacy_Accessibility"
+        return URL(string: privacyUrlString)
+    }
+
     public var body: some View {
         VStack {
             Button("Show Clipboards") {
                 NSApp.activate(ignoringOtherApps: true)
+            }
+            Text("Process is trusted: \(AXIsProcessTrusted() ? "true" : "false")")
+            Button("Open Privacy Settings") {
+                if let url = accessibilityUrl {
+                    NSWorkspace.shared.open(url)
+                }
             }
             RecordShortcutView()
             Button("Quit") {
