b5c74398313068075f94d72140d810049d25101c
Libec <libor.huspenina@gmail.com>
2022-10-23 19:41:06 +0200

    Fix plist with app being just an agent

--- App/Windows/Windows.swift
+++ App/Windows/Windows.swift
@@ -3,6 +3,23 @@ import ClipNinjaPackage
 import Combine
 import KeyboardShortcuts
 
+class SettingsWindow: NSWindow {
+
+    init() {
+        super.init(
+            contentRect: NSRect(x: 0, y: 0, width: 450, height: 300),
+            styleMask: [.titled, .closable],
+            backing: .buffered,
+            defer: true
+        )
+        title = "ClipNinja Settings"
+        backgroundColor = NSColor(Colors.prominent)
+        setFrameAutosaveName("SettingsWindow")
+        hidesOnDeactivate = true
+        isReleasedWhenClosed = false
+    }
+}
+
 class ClipboardWindow: NSWindow {
 
     let keySubject = PassthroughSubject<KeyboardShortcuts.Key, Never>()
