cdf30211fc17fcf65745a4eb4c91037c40db0044
Libec <libor.huspenina@gmail.com>
2022-12-30 16:16:25 +0100

    Store clips to protected file

--- ClipNinja/Sources/Features/Clipboard/System/ProtectedClipsStorage.swift
+++ ClipNinja/Sources/Features/Clipboard/System/ProtectedClipsStorage.swift
@@ -5,44 +5,53 @@ protocol ClipsStorage {
     var clips: [Clip] { get }
 }
 
-final class UserDefaultsClipsStorage: ClipsStorage {
+final class ProtectedClipsStorage: ClipsStorage {
 
     private let clipboardsStorageKey = "Clipboards"
 
     private let jsonDecoder: JSONDecoder
     private let jsonEncoder: JSONEncoder
-    private let userDefaults: UserDefaults
+    private let fileManager: FileManager
 
     init(
         jsonDecoder: JSONDecoder,
         jsonEncoder: JSONEncoder,
-        userDefaults: UserDefaults
+        fileManager: FileManager
     ) {
         self.jsonDecoder = jsonDecoder
         self.jsonEncoder = jsonEncoder
-        self.userDefaults = userDefaults
+        self.fileManager = fileManager
     }
 
     func persist(clips: [Clip]) {
+        let url = makeStorage()
         do {
             let data = try jsonEncoder.encode(clips)
-            userDefaults.set(data, forKey: clipboardsStorageKey)
+            try data.write(to: url, options: [.atomic, .completeFileProtection])
         } catch {
             log(message: "Cant encode clips: \(error)")
         }
     }
 
     var clips: [Clip] {
-        guard let data = userDefaults.data(forKey: clipboardsStorageKey) else {
-            log(message: "No data in user defaults")
-            return []
-        }
+        let url = makeStorage()
         do {
+            let data = try Data(contentsOf: url, options: [])
             let clips = try jsonDecoder.decode([Clip].self, from: data)
             return clips
         } catch {
             log(message: "Failed to decode clips: \(error)")
             return []
         }
+    }
+
+    private func makeStorage() -> URL {
+        guard let url = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else {
+            log(message: "Failed to make URL to store data")
+            fatalError()
+        }
+        let clipsUrl = url.appending(path: "clips")
+        log(message: clipsUrl.path)
+        return clipsUrl
     }
 }
